1cc402ee3bef273c05e555df2e8793e874e6322b
sam <md346@hotmail.com>
2019-06-07 14:08:07 +0200

    Api Connection, UseEffect Implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 import Error from './componentes/Error';
@@ -11,6 +11,7 @@ function App () {
   const [ciudad, setCiudad] = useState('');
   const [pais, setPais] = useState('');
   const [error,setError] = useState(false);
+  const [resultado, setResultado] = useState({});
 
   // 1)datos que el usuario quiere consultar, creamos un metodo y luego lo pasamos como prop por el formulario
   const datosConsulta = (datos) =>{
@@ -26,6 +27,35 @@ function App () {
     setError(false);
     
   };
+
+  // useEffect toma el lugar de component didmount y component didupdate
+  useEffect (() => {
+    // prevenimos primera ejecucion del metodo consultarAPI() para evitar errores 404, not found.
+    if (ciudad === '') return;
+
+    // Incluimos el metodo de consulta de API dentro del UseEffect
+    // Metodo para la consulta de la API, opteniendo objetos en formato JSON.
+  const consultarAPI = async () =>{
+
+    // La Key proporcionada por la API
+    const appId = '560661190727bf4b592da1703744076a';
+    // La url para llamar a la api
+    const url = ` https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    // consultar la URL
+
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+
+    setResultado(resultado);
+
+  
+  }
+
+  consultarAPI();
+  }, [ciudad, pais] );
+
+  
 
   // Cargamos un componente de manera condicional, aqui hacemos la logica para renderizar el componente Error, le pasamos un mensaje como parametro y usamos la variable COMPONENTE que declaramos para retornarla debidamente maquetada posteriormente.
   let componente;
